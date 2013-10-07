baa8a008cb66ee65589318ae946c1403f71c25d1
Ralph Bolo <ralph.bolo@gmail.com>
2013-10-07 13:23:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ragetwits::Application.config.secret_key_base = 'd001340722538979ce5929ea5772c688c13d93aaaca2b7b4b873c06316ecf85d4187c87f7abc399d9c7735e81696545e9d145d89bd99e7d2950a6d075b592305'
